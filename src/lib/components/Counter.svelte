b92ae26acf2140547c049d6bbe058a2ff8380148
Bobocan <bobocan@bobocan.xyz>
2025-12-26 15:36:24 +0700

    Add Counter

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  let value = $state(0);
+</script>
+
+<button type="button" class="border px-2 py-1 cursor-pointer rounded-md" onclick={() => value++}>
+  Click me: {value}
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script>
+  import Counter from "$lib/components/Counter.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+
+<div class="flex gap-4">
+  <Counter></Counter>
+  <Counter></Counter>
+  <Counter></Counter>
+</div>
+
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
