a5a6a1fb68fa335f13d17a441a08f4b4318f79f2
Bobocan <bobocan@bobocan.xyz>
2025-12-26 15:40:33 +0700

    Add About page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,9 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<header class="sticky top-0 flex gap-4 px-4 py-2 border-b">
+	<a href="/" class="text-blue-600 hover:underline">Home</a>
+	<a href="/about" class="text-blue-600 hover:underline">About</a>
+</header>
+
 {@render children()}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Counter from "$lib/components/Counter.svelte";
+</script>
+
+<h1>About</h1>
+
+<Counter></Counter>
