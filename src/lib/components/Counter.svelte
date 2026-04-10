6a5018dfb0838ab894834573dc0b4ba9ca808e74
Bobocan <bobocan@bobocan.xyz>
2026-04-10 16:01:36 +0700

    Show sync counter

--- src/lib/components/Counter.svelte
+++ src/lib/components/Counter.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-  let value = $state(0);
+  let {
+    value = $bindable(0),
+  }: {
+    value?: number;
+  } = $props();
 </script>
 
 <button type="button" class="border px-2 py-1 cursor-pointer rounded-md" onclick={() => value++}>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
   import Counter from "$lib/components/Counter.svelte";
+
+  let value = $state(0);
+
+  let valueSync = $state(0);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
-<div class="flex gap-4">
-  <Counter></Counter>
-  <Counter></Counter>
-  <Counter></Counter>
+<div class="grid gap-4">
+  <div class="flex gap-4">
+    <Counter></Counter>
+  </div>
+
+  <div class="flex gap-4">
+    <Counter bind:value></Counter>
+    <div>The counter value is {value}</div>
+  </div>
+
+  <div class="flex gap-4">
+    <Counter bind:value={valueSync}></Counter>
+    <Counter bind:value={valueSync}></Counter>
+  </div>
 </div>
 
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
